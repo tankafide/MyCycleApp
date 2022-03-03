9536950256867590328758abc4a6806de97370c7
Shane Stewart <tankafide@gmail.com>
2022-03-03 17:37:00 -0600

    structure setup

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,13 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onboardingDomain = ":onboarding:onboarding_domain"
+    const val onboardingPresentation = ":onboarding:onboarding_presentation"
+
+    const val rideTrackingData = ":rideTracking:rideTracking_data"
+    const val rideTrackingDomain = ":rideTracking:rideTracking_domain"
+    const val rideTrackingPresentation = ":rideTracking:rideTracking_presentation"
+}
